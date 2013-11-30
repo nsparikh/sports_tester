07502a7c4a09618e419c71341ba8b19c55296883
Neena Parikh <nparikh92@gmail.com>
2013-11-30 17:48:05 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SportsTester::Application.config.secret_key_base = '455abfb9f427fba37c410e05df8806302e759e6a1aa62d3b9c49c7e587f931d750a52c23cf21d977fd7366061fb0c26a04ab5ea18f17dccd249750fedb0813d9'

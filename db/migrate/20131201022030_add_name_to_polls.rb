1cc34a362ccdbd2c9820cfb74c322a259f009890
Neena Parikh <nparikh92@gmail.com>
2013-11-30 21:30:37 -0500

    Modify polls structure

--- db/migrate/20131201022030_add_name_to_polls.rb
+++ db/migrate/20131201022030_add_name_to_polls.rb
@@ -1,6 +1,6 @@
 class AddNameToPolls < ActiveRecord::Migration
   def change
     add_column :polls, :teamAName, :string
-    add_column :polls, :teamBName, :String
+    add_column :polls, :teamBName, :string
   end
 end
